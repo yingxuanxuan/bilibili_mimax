89702c25bff75da08dc1329a30612f3a70ca189e
yingxuanxuan <yingxuanxuan@hotmail.com>
2016-05-19 22:55:55 +0800

    Auto reload set active the first page on minute 50 and close others.

--- bilibili_mimax/background.js
+++ bilibili_mimax/background.js
@@ -3,18 +3,27 @@ setInterval(function(){
 	var hour = date.getHours();
 	var minute = date.getMinutes();
 	console.log("Now：" + date.toLocaleString());
+
 	if ( hour > 12 && hour < 24 && minute < 60 && minute > 49){
-		chrome.tabs.query(
-			{url:"http://live.bilibili.com/1"}, 
-			function(tabs){
+		chrome.tabs.query({url:"http://live.bilibili.com/1"}, function(tabs){
 				if (tabs.length < 1)
 				{
 					console.log("return " + tabs.length)
-					console.log("Open.")
+					console.log("Open new.")
 					chrome.tabs.create({url:"http://live.bilibili.com/1"});
 				}else{
 					console.log("Opened " + tabs.length)
+					if (50 == minute)
+					{
+						console.log("Reload and set active the first one, close others.")
+						for(var i = 1; i < tabs.length; i++)
+						{
+							chrome.tabs.remove(tabs[i].id);
+						}
+						chrome.tabs.reload(tabs[0].id);
+						chrome.tabs.update(tabs[0].id, {active:true});
+					}
 				}
 		});
 	}
-}, 30 * 1e3);
+}, 60 * 1e3);
